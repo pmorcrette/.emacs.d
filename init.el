09494d2e3e3bcd346b7122ae8238790d8c301f3e
Pierre Morcrette <git.pmorcrette@pm.me>
2024-10-13 00:02:47 +0200

    Python integration

--- init.el
+++ init.el
@@ -230,3 +230,20 @@ s
 (use-package shfmt
   :hook
   (sh-mode . shfmt-on-save-mode))
+
+;; Snake incoming !
+
+;; Config for python
+(use-package python-mode
+  :ensure t
+  :hook
+  (python-mode . lsp-deferred))
+
+(use-package lsp-pyright
+  :ensure t
+  :hook
+  (python-mode .
+	       (lambda ()
+		 (require 'lsp-pyright)
+		 (lsp-deferred)))) ; or lsp-deferred
+
